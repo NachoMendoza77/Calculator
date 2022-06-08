6a3196187840a0aa9f98295ccb22d0f00e42a70f
Ignacio-Mendoza <mendozaignacio9228@gmail.com>
2022-06-08 11:15:30 -0300

    Calculadora

---
+++ JS/calculadora.js
@@ -0,0 +1,42 @@
+window.addEventListener('load', ()=> {
+    const display = document.querySelector('.cal-display');
+    const keyButtons = document.getElementsByClassName('key-button');
+
+    const keyButtonsArray = Array.from(keyButtons);
+
+    keyButtonsArray.forEach( (button) =>{
+        button.addEventListener('click', ()=> {
+            calculadora(button, display);
+        })
+    })
+});
+
+function calculadora(button, display){
+    switch(button.innerHTML){
+        case 'C':
+            borrar(display);
+            break;
+        case '=':
+            calcular(display);
+            break;
+
+        default:
+            actualizar(display, button);
+            break;
+    }
+}
+
+function calcular(display){
+    display.innerHTML = eval(display.innerHTML)
+}
+
+function actualizar(display, button){
+    if (display.innerHTML == 0){
+        display.innerHTML = '';
+    }
+    display.innerHTML += button.innerHTML;
+}
+
+function borrar(display){
+    display.innerHTML = 0;
+}
